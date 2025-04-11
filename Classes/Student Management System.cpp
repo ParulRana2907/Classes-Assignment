04274e811e2a3d65cc1025d68a715bb75bf5e3fe
Parul Rana <parulrana2407@gmail.com>
2025-04-11 20:55:50 +0530

    Student Management System.cpp
    .cpp

--- Classes/Student Management System.cpp
+++ Classes/Student Management System.cpp
@@ -1,1 +1,92 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+const int MAX_COURSES = 10, MAX_STUDENTS = 100;
+class Student {
+    string name, roll, courses[MAX_COURSES];
+    double cgpa;
+    int courseCount = 0; 
+
+public:
+    Student() : cgpa(0) {}
+    Student(string n, string r, double g) : name(n), roll(r), cgpa(g) {}
+
+    void addCourse(string course) {
+        if (courseCount < MAX_COURSES) {
+            courses[courseCount++] = course;
+        } else {
+            cout << "Course limit reached!\n";
+        }
+    }
+
+    void updateCGPA(double newCGPA) {
+        if (newCGPA >= 0 && newCGPA <= 4) {
+            cgpa = newCGPA;
+        } else {
+            cout << "Invalid CGPA!\n";
+        }
+    }
+
+    void display() const {
+        cout << "Name: " << name << "\nRoll: " << roll << "\nCGPA: " << cgpa << "\nCourses: ";
+        for (int i = 0; i < courseCount; ++i) {
+            cout << courses[i] << " ";
+        }
+        cout << endl;
+    }
+
+    string getRoll() const { return roll; }
+};
+
+class StudentManagementSystem {
+    Student students[MAX_STUDENTS];
+    int studentCount = 0; 
+
+public:
+    void addStudent(const Student& student) {
+        if (studentCount < MAX_STUDENTS) {
+            students[studentCount++] = student;
+        } else {
+            cout << "Student limit reached!\n";
+        }
+    }
+
+    Student* searchStudent(string roll) {
+        for (int i = 0; i < studentCount; ++i) {
+            if (students[i].getRoll() == roll) {
+                return &students[i];
+            }
+        }
+        return nullptr; // 
+    }
+
+    void displayAll() const {
+        if (studentCount == 0) {
+            cout << "No students found.\n";
+            return;
+        }
+        for (int i = 0; i < studentCount; ++i) {
+            students[i].display();
+        }
+    }
+};
+
+int main() {
+    StudentManagementSystem sms;
+    sms.addStudent({"Harry", "1", 7.5});
+    sms.addStudent({"Jasmine", "2", 3.9});
+
+    Student* student1 = sms.searchStudent("1");
+    if (student1) {
+        student1->addCourse("Math");
+    }
+    Student* student2 = sms.searchStudent("2");
+    if (student2) {
+        student2->updateCGPA(4.0);
+        student2->addCourse("Science");
+    }
+    sms.displayAll();
+
+    return 0;
+}
